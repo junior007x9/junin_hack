dc89a1ee7329aec700d8d99ac03090187d8d2223
junior007x9 <147353899+junior007x9@users.noreply.github.com>
2024-07-11 14:16:24 -0300

    Delete ativudade 12 directory

--- ativudade 12/elemento.js
+++
@@ -1,5 +0,0 @@
-function mostravalor(elemento) {
-  var idade = elemento.getAttribute("data-idade");
-  var cpf = elemento.getAttribute("data-cpf");
-  console.log("Idade: " + idade + ", CPF: " + cpf);
-}
